a17a5a85e7a2811a774545121b9f9ddb1f576e96
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-28 08:57:43 -0500

    sync database command

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const { Department, Role, Employee } = require("./Models");
 const sequelize = require("./connection");
 
 const inquirer = require("inquirer");
+
+sequelize.sync({ force: false }).then(() => {
+    options();
+});
