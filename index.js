a579c69f90986d25f7eb8d6168377ba33e2eae69
DigitallyIntrinsic <139934612+DigitallyIntrinsic@users.noreply.github.com>
2023-10-28 08:59:21 -0500

    user options prompt in CLI

--- index.js
+++ index.js
@@ -7,3 +7,40 @@ const inquirer = require("inquirer");
 sequelize.sync({ force: false }).then(() => {
     options();
 });
+
+function options() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What would you like to do?",
+          choices: [
+            "View All Departments",
+            "View All Roles",
+            "View All Employees",
+            "Add Department",
+            "Add Role",
+            "Add Employee",
+            "Update Employee Role",
+          ],
+          name: "employeeTracker",
+        },
+      ])
+    .then((answer) => {
+        if (answer.employeeTracker === "View All Departments") {
+          viewAllDepartments();
+        } else if (answer.employeeTracker === "View All Roles") {
+          viewAllRoles();
+        } else if (answer.employeeTracker === "View All Employees") {
+          viewAllEmployees();
+        } else if (answer.employeeTracker === "Add Department") {
+          addDepartment();
+        } else if (answer.employeeTracker === "Add Role") {
+          addRole();
+        } else if (answer.employeeTracker === "Add Employee") {
+          addEmployee();
+        } else {
+          updateEmployeeRole();
+        }
+      });
+  }
